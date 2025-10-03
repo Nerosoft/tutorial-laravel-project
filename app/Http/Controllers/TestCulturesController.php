fad9ed12922dd4b69e3a7f1c005d0b9950ddabff
unknown <ggamals7744@yahoo.com>
2025-10-03 10:21:31 +0300

    tutorial project 2025

--- app/Http/Controllers/TestCulturesController.php
+++ app/Http/Controllers/TestCulturesController.php
@@ -43,8 +43,7 @@ class TestCulturesController extends Page implements TableData
         $this->message['input-output-lab.required'] = $this->error4;
         $this->message['input-output-lab.in'] = $this->getDb()[$this->getDb()['Setting']['Language']][request()->route('id')]['InputOutputLabInvalid'];
         $arr = (array)$this->getDb()[request()->route('id')];
-        $myKey = Route::currentRouteName() === 'editTest' ?request()->input('id'):$this->generateUniqueIdentifier();
-        $arr[$myKey] = array('Name'=>request()->input('name'), 'Shortcut'=>request()->input('shortcut'), 'Price'=>request()->input('price'), 'InputOutputLab'=>request()->input('input-output-lab'), 'Id'=>$myKey);
+        $arr[Route::currentRouteName() === 'editTest' ?request()->input('id'):$this->generateUniqueIdentifier()] = array('Name'=>request()->input('name'), 'Shortcut'=>request()->input('shortcut'), 'Price'=>request()->input('price'), 'InputOutputLab'=>request()->input('input-output-lab'), 'Id'=>Route::currentRouteName() === 'editTest' ?request()->input('id'):$this->generateUniqueIdentifier());
         $this->getDb()[request()->route('id')] = $arr;
     }
     public function __construct(){
