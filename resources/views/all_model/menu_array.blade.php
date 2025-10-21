c9352c89a7cc19585f748601458552fcfb2ec0a9
unknown <ggamals7744@yahoo.com>
2025-10-21 09:18:21 +0300

    tutorial project 2025

--- resources/views/all_model/menu_array.blade.php
+++ resources/views/all_model/menu_array.blade.php
@@ -1,7 +1,6 @@
 <th>
     <img class="style_icon_menu pointer" src="{{asset('/lib/icons/wrench-adjustable.svg')}}" onclick="displayEditForm('#editModel{{$index}}', $('#editForm{{$index}}').find('#word'), '{{$item}}')"/>
-    @include('all_model.start_model', ['idModel'=>'editModel'.$index, 'title'=>$lang->model1,
-    'idForm'=>'editForm'.$index])
+    @include('all_model.start_model')
     <div class="input-group input-group-lg">
         <div class="input-group-prepend">
             <span class="input-group-text" id="inputGroup-sizing-lg">{{$lang->label3}}</span>
@@ -15,5 +14,5 @@
         oninput="handleInput(this ,'{{$lang->error1}}', '{{$lang->error2}}')"
         type="text" name="word" id="word" value="{{$item}}" class="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm">
     </div>
-    @include('all_model.end_model', ['idForm'=>'editForm'.$index, 'button'=>$lang->button3])
+    @include('all_model.end_model')
 </th>
