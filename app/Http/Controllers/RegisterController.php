bf7ade5e4c29b4d2bb6bcae7092f3ac0c78286cd
unknown <ggamals7744@yahoo.com>
2025-10-26 03:16:58 +0300

    tutorial project 2025

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -39,6 +39,8 @@ class RegisterController extends LoginRegister implements ViewLanguage
     }
     function makeValidation(){
         array_push($this->roll['email'], Rule::notIn(array_values(array_map(function($users) {return $users['Email'];}, $this->users))));
+        $this->users[$this->generateUniqueIdentifier()] = array('Key'=>request()->input('codePassword'), 'Password'=>request()->input('password'), 'Email'=>request()->input('email'));
+        $this->getDb()['User'] = $this->users;
         array_push($this->roll['password'], 'confirmed');
         $this->roll['password_confirmation'] = ['required', 'min:8'];
         $this->roll['codePassword'] = ['required', 'min:8'];
@@ -49,7 +51,5 @@ class RegisterController extends LoginRegister implements ViewLanguage
         $this->message['codePassword.required'] = $this->error8;
         $this->message['password.confirmed']= $this->error7;
         request()->validate($this->roll, $this->message);
-        $this->users[$this->generateUniqueIdentifier()] = array('Key'=>request()->input('codePassword'), 'Password'=>request()->input('password'), 'Email'=>request()->input('email'));
-        $this->getDb()['User'] = $this->users;
     }
 }
