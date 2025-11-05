75efca3427c13cab09226c46f2f325a12615daef
unknown <ggamals7744@yahoo.com>
2025-11-05 21:33:14 +0200

    tutorial project 2025

--- app/Http/Controllers/SettingPage.php
+++ app/Http/Controllers/SettingPage.php
@@ -7,8 +7,12 @@ use Illuminate\Http\Request;
 class SettingPage extends Controller
 {
     function __construct($language, $title = null, $direction = null){
-        $this->language = $language;
-        $this->title = $title;
-        $this->direction = $direction;
+        if($title === null && $direction === null){
+            $this->language = $language;
+        }else{
+            $this->language = $language;
+            $this->title = $title;
+            $this->direction = $direction;
+        }
     }
 }
