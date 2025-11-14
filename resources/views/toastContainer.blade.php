c11094df43f64dbec2dcefd8fea32455f9bc53c0
unknown <ggamals7744@yahoo.com>
2025-11-14 11:41:48 +0200

    tutorial project 2025

--- resources/views/toastContainer.blade.php
+++ resources/views/toastContainer.blade.php
@@ -1,10 +1,5 @@
  <div id="{{$key}}" class="toast align-items-center text-bg-{{$type}} border-0 mt-2" role="alert" aria-live="assertive" aria-atomic="true">
-    <div class="d-flex">
-        <div class="toast-body">{{ $toast }}</div>
-        <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
-    </div>
-</div>
- <script>
+    <script>
    
     (new bootstrap.Toast(
          $('#'+@json($key)).on("hidden.bs.toast", function () {
@@ -13,3 +8,10 @@
         
         , { delay: 10000 })).show();
 </script>
+ <div class="d-flex">
+        <div class="toast-body">{{ $toast }}</div>
+        <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+    </div>
+     
+</div>
+
