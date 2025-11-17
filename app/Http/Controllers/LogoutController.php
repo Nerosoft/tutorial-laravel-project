8f1ca1b40a8ab3fd71dcc0543f338825a97fcca3
unknown <ggamals7744@yahoo.com>
2025-11-17 07:45:50 +0200

    tutorial project 2025

--- app/Http/Controllers/LogoutController.php
+++ app/Http/Controllers/LogoutController.php
@@ -9,9 +9,9 @@ use App\Models\mydb;
 class LogoutController extends Controller
 {
     function logout(){
-        $url = mydb::first()['_id'] === request()->session()->get('superId') ? '/login' : '/login/'.request()->session()->get('superId');
+        $url = mydb::first()['_id'] === request()->session()->get('superId') ? redirect()->route('mylogin') : redirect('/login/'.request()->session()->get('superId'));
         request()->session()->forget('superId');
         request()->session()->forget('userId');
-        return redirect($url);
+        return $url;
     }
 }
