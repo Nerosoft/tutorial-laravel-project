b55a00e0b6b8e07935cfa04d6b4401ab2816b6b9
unknown <ggamals7744@yahoo.com>
2025-11-25 13:13:22 +0200

    tutorial project 2025

--- app/Http/Controllers/FlexTableController.php
+++ app/Http/Controllers/FlexTableController.php
@@ -28,18 +28,15 @@ class FlexTableController extends Page implements TableData
             $this->message[$key.'.min'] = $this->ErrorsMessageInv[$key];
         }
         $arr = (array)$this->getDb()[request()->route('id')];
-        if(Route::currentRouteName() === 'createFlexTable'){
+        $keyId;
+        if(Route::currentRouteName() === 'createFlexTable')
             $keyId = $this->generateUniqueIdentifier();
-            $arr[$keyId] = array();
-            foreach ($this->MyInfo()[request()->route('id')]['TableHead'] as $key => $value)
-                $arr[$keyId][$key] = request()->input($key);
-        }
         else{
             $this->successfulyMessage = $this->MyInfo()[request()->route('id')]['MessageModelEdit'];
-            $arr[request()->input('id')] = array();
-            foreach ($this->MyInfo()[request()->route('id')]['TableHead'] as $key => $value)
-                $arr[request()->input('id')][$key] = request()->input($key);
+            $keyId = request()->input('id');
         }
+        foreach ($this->MyInfo()[request()->route('id')]['TableHead'] as $key => $value)
+            $arr[$keyId][$key] = request()->input($key);
         request()->validate($this->roll, $this->message);
         $this->getDb()[request()->route('id')] = $arr;
     }
