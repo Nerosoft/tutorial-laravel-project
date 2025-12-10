825f5c63e186ae325bd78ac17b92d7f015c4fd66
unknown <ggamals7744@yahoo.com>
2025-12-10 09:39:49 +0200

    tutorial project 2025

--- resources/views/all_language.blade.php
+++ resources/views/all_language.blade.php
@@ -22,12 +22,12 @@
                     @foreach($data as $key=>$myData)
                         @foreach($myData as $key2=>$items)
                             @if(is_array($items))
-                                @foreach($items as $key3=>$item)
+                                @foreach($items as $key3=>$items)
                                     <tr>
                                         <th>{{$index++}}</th>
                                         <th>{{$lang->getDataTable()[$lang->language]['AllNamesLanguage'][$myNameLang]}}</th>
-                                        <th>{{$item}}</th>
-                                        @include('all_model.menu_array', ['idModel'=>'editModel'.$index, 'title'=>$lang->title3, 'idForm'=>'editForm'.$index, 'button'=>$lang->button3, 'action'=>route('edit.editAllLanguage', ['lang'=>$myNameLang, 'id'=>$key, 'name'=>$key2, 'item'=>$key3])])
+                                        <th>{{$items}}</th>
+                                        @include('all_model.table_array', ['idModel'=>'editModel'.$index, 'title'=>$lang->title3, 'idForm'=>'editForm'.$index, 'button'=>$lang->button3, 'action'=>route('edit.editAllLanguage', ['lang'=>$myNameLang, 'id'=>$key, 'name'=>$key2, 'item'=>$key3])])
                                     </tr>
                                 @endforeach
                             @elseif($key === 'Html')
@@ -52,11 +52,11 @@
             @else
                 @foreach($lang->getDataTable() as $key=>$items)
                     @if(is_array($items))
-                        @foreach($items as $key3=>$item)
+                        @foreach($items as $key3=>$items)
                             <tr>
                                 <th>{{$index++}}</th>
-                                <th>{{$item}}</th>
-                                @include('all_model.menu_array', ['idModel'=>'editModel'.$index, 'title'=>$lang->title3, 'idForm'=>'editForm'.$index, 'button'=>$lang->button3, 'action'=>route('edit.editAllLanguage', ['lang'=>request()->route('lang'), 'id'=>request()->route('id'), 'name'=>$key, 'item'=>$key3])])
+                                <th>{{$items}}</th>
+                                @include('all_model.table_array', ['idModel'=>'editModel'.$index, 'title'=>$lang->title3, 'idForm'=>'editForm'.$index, 'button'=>$lang->button3, 'action'=>route('edit.editAllLanguage', ['lang'=>request()->route('lang'), 'id'=>request()->route('id'), 'name'=>$key, 'item'=>$key3])])
                             </tr>
                         @endforeach
                     @elseif(request()->route('id') === 'Html')
